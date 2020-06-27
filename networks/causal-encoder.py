52cd5fc1bda2106da3918cbc87fdc0d00d878d55
Yasa Baig <yasabaig@yasas-mbp.myfiosgateway.com>
2020-06-27 02:24:53 -0400

    Add documentation to causal-encoder

--- networks/causal-encoder.py
+++ networks/causal-encoder.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+    Provides CausalCNN and CausalCNNEncoder classes to enable users to generate robust representations of 
+    multivariate time series data. 
 
+    The following method is based on the paper from Franceschi, Dieuleveut, and Jaggi (2020) and published in 
+    NeurIPS (https://arxiv.org/pdf/1901.10738v4.pdf). The general idea is that through the use of casually diluted
+    convolution operation inserted into an encoder coupled with a unique negative-sampling based triplet loss, 
+    one can generate robust representations of time series without labels and of customizable dimension. Here 
+    we implement their model as well as some PyTorch helper classes to help in management of the tensor sizes. The 
+    details of their models are described in the associated readme. 
+"""
